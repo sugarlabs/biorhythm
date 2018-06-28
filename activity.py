854a5c6bbfe9ac908f3571f1e05aea2ffc16f22e
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2018-06-29 01:17:00 +0530

    Add license to activity.py
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

--- activity.py
+++ activity.py
@@ -1,6 +1,24 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright 2012-18, Alan Aguiar
+
+# This file is part of the Biorhythm Activity.
+
+# The Biorhythm Activity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# The Biorhythm Activity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with the Biorhythm Activity.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('PangoCairo', '1.0')
