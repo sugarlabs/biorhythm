e3f4dfab9cccc83bf2ba22c449384c30f0207796
Vipul Gupta <vipulgupta2048@gmail.com>
2018-07-17 03:59:18 +0530

    Add copyright, add license, .gitignore, .flake8 and metadata
    * add copyright and license to source, with consent from Alan Aguiar,
    
    * add license to repository, to comply with license offer,
    
    * add to .gitignore,
    
    * fix Flake8 issue and add a .flake8 file,
    
    * change metadata summary, classify as a pseudoscience; research has concluded the theory of biorhythms does not work, it is easily tested and has been shown to be false.  But supporters continue to defend, so the theory is pseudoscience.
    
    * add metadata tags _Math_ and _Time_.

--- activity.py
+++ activity.py
@@ -1,6 +1,23 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2012-17, Alan Aguiar
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# This file is part of the Biorhythm Activity.
+
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('PangoCairo', '1.0')
@@ -61,7 +78,7 @@ class Activity(activity.Activity):
 
         self.show_all()
 
-        #'alto', 'critico', 'bajo'
+        # 'alto', 'critico', 'bajo'
 
     def build_toolbar(self):
 
